f1308809b4d3920bea9de7fefc4ee006fc559545
dimanteo <dikolobay@gmail.com>
2024-05-19 17:31:52 +0300

    [Markov] Add run script

--- utils/download.py
+++ utils/download.py
@@ -1,20 +1,11 @@
-import sys
+from tracelist import get_traces, get_trace_path
 import subprocess
-import os
 
 def main():
-    if len(sys.argv) < 2:
-        print("Enter destination folder")
-        exit(-1)
-    tracelist = 'tracelist.txt'
-    if len(sys.argv) == 3:
-        tracelist = sys.argv[2]
-    with open(tracelist, 'r') as file:
-        traces = file.read().split()
     url = 'https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/'
-    for trace in traces:
+    for trace in get_traces():
         trace_url = url + trace
-        dst_path = os.path.join(sys.argv[1], trace)
+        dst_path = get_trace_path(trace)
         subprocess.run(['curl', trace_url, '-s', '-o', dst_path])
 
 if __name__ == '__main__':
---
+++ utils/runsim.py
@@ -0,0 +1,16 @@
+from tracelist import get_traces, get_trace_path
+import subprocess
+
+def main():
+    warmup = 5000000
+    simulation = 20000000
+    for trace in get_traces():
+        subprocess.run([
+            '../bin/champsim',
+            '--warmup-instructions', str(warmup),
+            '--simulation-instructions', str(simulation),
+            get_trace_path(trace)
+        ])
+
+if __name__ == '__main__':
+    main()
