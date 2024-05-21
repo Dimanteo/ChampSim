81d05630ff40009d4cd78db5abdfe5e3bdebbe22
dimanteo <dikolobay@gmail.com>
2024-05-21 15:28:10 +0300

    [Markov] Update running script

--- utils/runsim.py
+++ utils/runsim.py
@@ -5,10 +5,11 @@ def main():
     warmup = 5000000
     simulation = 20000000
     for trace in get_traces():
-        subprocess.run([
+        subprocess.Popen([
             '../bin/champsim',
             '--warmup-instructions', str(warmup),
             '--simulation-instructions', str(simulation),
+            '--json', trace + '_stats.json',
             get_trace_path(trace)
         ])
 
--- utils/tracelist.py
+++ utils/tracelist.py
@@ -11,4 +11,7 @@ def get_traces(tracelist='tracelist.txt'):
         return file.read().split()
 
 def get_trace_path(trace):
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} benchmarks/folder [tracelist.txt]")
+        exit(-1)
     return os.path.join(sys.argv[1], trace)
